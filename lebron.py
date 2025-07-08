84e5ec1c97c27c61abc4d7647af23426836ba37b
tis a secret <125412609+J3inx@users.noreply.github.com>
2025-07-08 11:21:56 -0400

    w

---
+++ lebron.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import requests
+from io import BytesIO
+
+
+answer = input("who's the goat?")
+
+if answer.lower() in ['bron', 'lebron']:
+        print("your damn right")
+else:
+        print("wrong it's LEBRONNNN")
+        response = requests.get("https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png", stream=True)
+        im = Image.open(response.raw)
+        im.show()
